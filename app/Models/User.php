3dc3a2bb86cd578abdd26eac4207c3b400105626
ImanKHzaz <iman.kh1962@gmail.com>
2026-03-26 19:38:56 +0200

    Add Spatie Laravel Permission package and update User model
    - Added the Spatie Laravel Permission package (version 6.25) to composer.json and composer.lock.
    - Updated User model to include a property annotation for orders.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+/** @property \Illuminate\Database\Eloquent\Collection $orders */
+
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
